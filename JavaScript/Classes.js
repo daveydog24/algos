de3d5fb526aa112e63a75aa644f651c06545d27d
David Wukelic <david.wukelic@gmail.com>
2019-03-20 20:28:13 -0700

    finished Polygon perimeter method

--- JavaScript/Classes.js
+++ JavaScript/Classes.js
@@ -33,3 +33,22 @@ let triangle = new Polygon([3, 4, 5]);
 console.log(triangle.perimeter());
 */
 
+/*
+ * Implement a Polygon class with the following properties:
+ * 1. A constructor that takes an array of integer side lengths.
+ * 2. A 'perimeter' method that returns the sum of the Polygon's side lengths.
+ */
+
+class Polygon {
+    constructor(arr) {
+        this.arr = arr;
+    }
+    perimeter() {
+        let sum = 0;
+        for (let num of this.arr) {
+            // console.log(num);
+            sum += num;
+        }
+        return sum;
+    }
+}
