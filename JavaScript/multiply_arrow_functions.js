0caf25435fbce520a0f4643404068d1c5b74bb56
David Wukelic <david.wukelic@gmail.com>
2019-03-24 22:01:48 -0700

    finished algo

--- JavaScript/multiply_arrow_functions.js
+++ JavaScript/multiply_arrow_functions.js
@@ -48,3 +48,24 @@ are multipled by . In other words, . We then return
 the modified array as our answer.
 
 ```
+/*
+ * Modify and return the array so that all even elements are doubled and all odd elements are tripled.
+ * 
+ * Parameter(s):
+ * nums: An array of numbers.
+ */
+function modifyArray(nums) {
+    let tempNum = nums;
+
+    for (let i = 0; i < nums.length; i++){
+        if (nums[i] % 2 == 0 ) {
+            tempNum[i] = (nums[i] * 2);
+        } else {
+            tempNum[i] = (nums[i] * 3);
+
+        }
+    }
+
+    return(tempNum);
+    
+}
