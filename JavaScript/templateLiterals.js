25b3e92e2a2273da74aabe8a843c3f051283637c
David Wukelic <david.wukelic@gmail.com>
2019-03-24 21:58:19 -0700

    added algo instructions

--- JavaScript/templateLiterals.js
+++ JavaScript/templateLiterals.js
@@ -50,4 +50,18 @@ When we plug those values into our formula, we get the following:
 We then store these values in an array, [14, 10], sort the array, and 
 return the sorted array, [10, 14], as our answer.
 
+/*
+ * Determine the original side lengths and return an array:
+ * - The first element is the length of the shorter side
+ * - The second element is the length of the longer side
+ * 
+ * Parameter(s):
+ * literals: The tagged template literal's array of strings.
+ * expressions: The tagged template literal's array of expression values (i.e., [area, perimeter]).
+ */
+function sides(literals, ...expressions) {
+    
+}
+
+
 ```
